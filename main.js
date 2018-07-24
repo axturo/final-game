0feb041d48fb8048f930d3e0deaf14860d769e3c
gabriel <gabriel.cabrera2020@gmail.com>
2018-07-24 12:40:35 -0700

    added 3 more enemies

--- main.js
+++ main.js
@@ -55,20 +55,37 @@ function create() {
     ledge2.body.immovable = true;
     ledge3.body.immovable = true;
     
-    enemy = game.add.sprite(400, 400, 'enemy');
-    enemy.animations.add('left', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], true);
-    enemy.animations.add('right', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], true);
+    enemy = game.add.sprite(550, 390, 'enemy');
+    enemy.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
+    enemy.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
     enemy.scale.setTo(0.2, 0.2);
+    game.physics.arcade.enable(enemy);
+    enemy.body.gravity.y = 2000;
+    enemy.body.velocity.x = -100;
+    
+    enemy1 = game.add.sprite(100, 185, 'enemy');
+    enemy1.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
+    enemy1.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
+    enemy1.scale.setTo(0.2, 0.2);
+    game.physics.arcade.enable(enemy1);
+    enemy1.body.gravity.y = 2000;
+    enemy1.body.velocity.x = -125;    
+        
+    enemy2 = game.add.sprite(600, 85, 'enemy');
+    enemy2.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
+    enemy2.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
+    enemy2.scale.setTo(0.2, 0.2);
+    game.physics.arcade.enable(enemy2);
+    enemy2.body.gravity.y = 2000;
+    enemy2.body.velocity.x = -150;    
     
 }
 
 function update() {
     
-    game.physics.arcade.collide(this, platforms, function (enemy, platform) {
-        // if slime is moving to the right, 
-        // check if its position greater than the width of the platform minus its width
-        // if slime is moving to the left, 
-        // check if its position exceeds the left-most point of the platform
+    // enemy first ledge
+    
+    game.physics.arcade.collide(enemy, platforms, function (enemy, platform) {
         if (enemy.body.velocity.x > 0 && enemy.x > platform.x + (platform.width - enemy.width) ||
                 enemy.body.velocity.x < 0 && enemy.x < platform.x) {
             enemy.body.velocity.x *= -1; 
@@ -80,6 +97,36 @@ function update() {
         }
     });    
     
+    // enemy second ledge
+    
+    game.physics.arcade.collide(enemy1, platforms, function (enemy, platform) {
+        if (enemy1.body.velocity.x > 0 && enemy1.x > platform.x + (platform.width - enemy1.width) ||
+                enemy1.body.velocity.x < 0 && enemy1.x < platform.x) {
+            enemy1.body.velocity.x *= -1; 
+        } 
+        if (enemy1.body.velocity.x > 0) {
+            enemy1.animations.play('right');
+        } else {
+            enemy1.animations.play('left');
+        }
+    });    
+    
+    // enemy third ledge
+    
+    game.physics.arcade.collide(enemy2, platforms, function (enemy, platform) {
+        if (enemy2.body.velocity.x > 0 && enemy2.x > platform.x + (platform.width - enemy2.width) ||
+                enemy2.body.velocity.x < 0 && enemy2.x < platform.x) {
+            enemy2.body.velocity.x *= -1; 
+        } 
+        if (enemy2.body.velocity.x > 0) {
+            enemy2.animations.play('right');
+        } else {
+            enemy2.animations.play('left');
+        }
+    });    
+    
+//    game.physics.arcade.collide(enemy, platforms);
+ 
     game.physics.arcade.collide(player, ground);
     game.physics.arcade.collide(player, platforms);
     
