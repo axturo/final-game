9de191f832e0b2bb3840ded961f3d3a174caf39c
gabriel <gabriel.cabrera2020@gmail.com>
2018-07-25 09:56:28 -0700

    made it cleaner :)

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function preload() {
 
     game.load.spritesheet('mc', 'spreadsheets/running-animation.png', 220, 290);
     game.load.image('background', 'spreadsheets/background.png');
-    game.load.image('ground', 'spreadsheets/platform2.png');
+    game.load.spritesheet('ground', 'spreadsheets/animated-platform.png', 320, 60);
     game.load.spritesheet('enemy', 'spreadsheets/common-enemy.png', 250, 310);
 
 }
@@ -55,37 +55,30 @@ function create() {
     ledge2.body.immovable = true;
     ledge3.body.immovable = true;
     
-    enemy = game.add.sprite(550, 390, 'enemy');
-    enemy.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
-    enemy.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
-    enemy.scale.setTo(0.2, 0.2);
-    game.physics.arcade.enable(enemy);
-    enemy.body.gravity.y = 2000;
-    enemy.body.velocity.x = -100;
+    enemies = game.add.group();
     
-    enemy1 = game.add.sprite(100, 185, 'enemy');
-    enemy1.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
-    enemy1.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
-    enemy1.scale.setTo(0.2, 0.2);
-    game.physics.arcade.enable(enemy1);
-    enemy1.body.gravity.y = 2000;
-    enemy1.body.velocity.x = -125;    
-        
-    enemy2 = game.add.sprite(600, 85, 'enemy');
-    enemy2.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
-    enemy2.animations.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 10, true);
-    enemy2.scale.setTo(0.2, 0.2);
-    game.physics.arcade.enable(enemy2);
-    enemy2.body.gravity.y = 2000;
-    enemy2.body.velocity.x = -150;    
+    enemies.create(550, 390, 'enemy');
+    enemies.create(100, 185, 'enemy');
+    enemies.create(600, 85, 'enemy');
     
+    enemies.callAll('animations.add', 'animations', 'float', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 12, true)
+    
+    game.physics.arcade.enable(enemies);
+    
+    enemies.callAll('animations.play', 'animations', 'float');
+    enemies.forEach(function(enemy) {
+        enemy.scale.setTo(0.2, 0.2);
+        enemy.body.gravity.y = 2000;
+        enemy.body.velocity.x = -100;
+    });
+  
 }
 
 function update() {
     
-    // enemy first ledge
+//    game.physics.arcade.collide(enemies, platforms);
     
-    game.physics.arcade.collide(enemy, platforms, function (enemy, platform) {
+    game.physics.arcade.collide(enemies, platforms, function (enemy, platform) {
         if (enemy.body.velocity.x > 0 && enemy.x > platform.x + (platform.width - enemy.width) ||
                 enemy.body.velocity.x < 0 && enemy.x < platform.x) {
             enemy.body.velocity.x *= -1; 
@@ -96,37 +89,7 @@ function update() {
             enemy.animations.play('left');
         }
     });    
-    
-    // enemy second ledge
-    
-    game.physics.arcade.collide(enemy1, platforms, function (enemy, platform) {
-        if (enemy1.body.velocity.x > 0 && enemy1.x > platform.x + (platform.width - enemy1.width) ||
-                enemy1.body.velocity.x < 0 && enemy1.x < platform.x) {
-            enemy1.body.velocity.x *= -1; 
-        } 
-        if (enemy1.body.velocity.x > 0) {
-            enemy1.animations.play('right');
-        } else {
-            enemy1.animations.play('left');
-        }
-    });    
-    
-    // enemy third ledge
-    
-    game.physics.arcade.collide(enemy2, platforms, function (enemy, platform) {
-        if (enemy2.body.velocity.x > 0 && enemy2.x > platform.x + (platform.width - enemy2.width) ||
-                enemy2.body.velocity.x < 0 && enemy2.x < platform.x) {
-            enemy2.body.velocity.x *= -1; 
-        } 
-        if (enemy2.body.velocity.x > 0) {
-            enemy2.animations.play('right');
-        } else {
-            enemy2.animations.play('left');
-        }
-    });    
-    
-//    game.physics.arcade.collide(enemy, platforms);
- 
+
     game.physics.arcade.collide(player, ground);
     game.physics.arcade.collide(player, platforms);
     
